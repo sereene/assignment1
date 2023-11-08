2bfee242784a8bdd4d562f900cc981d696ab7d54
sereene <salad1223@naver.com>
2023-11-08 19:01:38 +0900

    hello

--- src/App.js
+++ src/App.js
@@ -1,15 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import Small from "./components/Small";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const MainBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 400px;
+  height: 100vh;
+  padding: 15px 15px 15px 15px;
+  background-color: rgb(120, 165, 255);
+`;
 
-      </header>
-    </div>
-  );
+const RightBox = styled.div`
+  display: flex;
+  align-items: flex-end;
+  flex-grow: 1;
+  justify-content: center;
+  width: 100px;
+  height: 100vh;
+  padding: 15px 15px 15px 15px;
+  background-color: lavender;
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+`;
+const Icon = styled.div`
+  display:flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+`
+
+const Title = styled.div`
+  display: flex;
+  align-items: end;
+  font-size: 40px;
+  color: white;
+  text-align: center;
+  font-family: Arial;
+  font-style: italic;
+  font-weight: bold;
+`;
+
+const LowBar=styled.div`
+  display: flex;
+  align-items: flex-end;
+  font-size: 40px;
+  color:white;
+`;
+
+
+const App = () => {
+    return (
+
+        <Container>
+            <MainBox>
+                <Title>Serin의</Title>
+                <Title>IT 블로그</Title>
+                <LowBar>-------------------------</LowBar>
+                <Icon>
+                <img src={"icon.jpg"} alt = "icon" width={50} />
+                <img src={"icon.jpg"} alt = "icon" width={50} />
+                <img src={"icon.jpg"} alt = "icon" width={50} />
+                <img src={"icon.jpg"} alt = "icon" width={50} />
+                </Icon>
+            </MainBox>
+            <RightBox>
+                <Small></Small>
+                <Small></Small>
+                <Small></Small>
+            </RightBox>
+
+        </Container>
+
+    )
+
 }
 
 export default App;
